bc6ff424581bc4f6705221e8465b0740abd1331d
joyelstephen <jsteph32@myune.edu.au>
2016-02-03 20:43:05 +1100

    update

--- wsn-api/register.php
+++ wsn-api/register.php
@@ -1,3 +1,68 @@
+<?php
+
+    //function that generates the API Key
+    function RandomString()
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randstring = '';
+        for ($i = 0; $i < 32; $i++) {
+            $randstring = $randstring . $characters[rand(0, strlen($characters))];
+        }
+        return $randstring;
+    }
+
+    if(isset($_POST["submit"]))
+    {
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $re_password = $_POST['re_password'];
+
+
+        //echo "md5 password: " . md5($password);
+        
+        if ($username&&$email&&$password&&$re_password) {
+            // TODO: here do some format checking for the email
+        
+            if($password != $re_password) {
+                $error = "<b>Password does not match.</b>";
+            }
+            else
+            {
+                //include database connection
+                include("core/connect.php");
+
+                // generate API Key and check if it exist generate new one
+                $flag = true;
+                //while(!$flag)
+                {
+                    $api = RandomString();
+                    
+                    //check it in the database
+                    //if found go to the loop and genrate new
+                    
+                    //esle set flag to flase
+                    // $flag = false;
+                }
+                
+                // now insert the data and the API-Key into the database
+                
+                
+                
+            }
+
+        }
+        else
+            $error = "<b>Please fill all fields.</b>";
+            
+    }   
+    
+    
+?>
+
+
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -16,34 +81,58 @@
         </nav>
         <article>
             <div id="three-fourth">
-                <h1>Registration!</h1>
-                <p>Pleas register below to get API Key.</p>
-                <form action="register.php" autocomplete="on" method="POST">
-                        <fieldset>
-                            <legend>Register:</legend>
-                            <table>
-                                <tr>
-                                    <td>User name: </td>
-                                    <td><input type="text" name="username"></td>
-                                </tr>
-                                <tr>
-                                    <td>Email:</td>
-                                    <td><input type="password" name="password"></td>
-                                </tr>
-                                <tr>
-                                    <td>Password:</td>
-                                    <td><input type="password" name="password"></td>
-                                </tr>
-                                <tr>
-                                    <td>Re-enter Password:</td>
-                                    <td><input type="password" name="password"></td>
-                                </tr>
-                            </table>
-                            <br>
-                            <input type="submit" value="Submit">
-                        </fieldset>
-                    </form>
-                <br>
+                <h1>Registration!</h1>                
+                <?php
+                    // error messager
+                    if(isset($error))
+                    {
+                        echo "$error <br>";
+                    }
+                ?>
+                
+                <?php
+                    if(isset($api))
+                    {
+                        echo "<form><fieldset><legend> API Key</legend>
+                               <table>
+                                <tr><td><h2>$api</h2>Please keep this API-Key in safe place. If you loos your API Key email us through the contact form. <br>
+                                </td></tr></table></fieldset></form>";
+                        
+                    } 
+                    else
+                    {
+                    ?>        
+                        <p>Pleas register below to get API Key.</p>
+                        <form action="register.php" autocomplete="on" method="POST">
+                                <fieldset>
+                                    <legend>Register:</legend>
+                                    <table>
+                                        <tr>
+                                            <td>User name: </td>
+                                            <td><input type="text" name="username"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Email:</td>
+                                            <td><input type="text" name="email"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Password:</td>
+                                            <td><input type="password" name="password"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Re-enter Password:</td>
+                                            <td><input type="password" name="re_password"></td>
+                                        </tr>
+                                    </table>
+                                    <br>
+                                    <input type="submit" value="Submit" name="submit">
+                                </fieldset>
+                            </form>
+                        <br>
+                    <?php
+                    }
+                    ?>
+               
             </div>
             <div id="one-fourth">
                 
