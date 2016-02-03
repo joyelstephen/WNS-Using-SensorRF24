bcb35043b3397a89a5617328e4d47d8ab06c5205
Joyel Shenes Stephen & Saif Almuqati <jsteph32@myune.edu.au>
2016-02-03 15:38:32 +1100

    Update connect.php

--- wsn-api/core/connect.php
+++ wsn-api/core/connect.php
@@ -1,14 +1,7 @@
 <?php
-<<<<<<< HEAD
-	/***********************************************************
-	 *  NOTE: edit the XXXXXX with correct credentials 
-	 ***********************************************************/
+    /***********************************************************
+     *  NOTE: edit the XXXXXX with correct credentials 
+     ***********************************************************/
     $dbconn = pg_connect("host=XXXXXXX dbname=XXXXXXX user=XXXXXXX password=XXXXXXX") or die("Could not connect to database. <a href='http://turing.une.edu.au/~jsteph32/wsn-api'>Go back</a><br>.");
 
 ?>
-=======
-
-    $dbconn = pg_connect("host=127.0.0.1 dbname=wsn_data user=jsteph32_apps password=220130353") or die("Could not connect to database. <a href='http://turing.une.edu.au/~jsteph32/wsn-api'>Go back</a><br>.");
-
-?>
->>>>>>> 3c2a33f5b6387c6c2f20fa25923b7449b877b967
