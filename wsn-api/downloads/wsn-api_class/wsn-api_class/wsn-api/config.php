245a8d643f387429bb9f52b059da3b4b5dd6dc29
joyelstephen <jsteph32@myune.edu.au>
2016-02-03 20:36:05 +1100

    update

---
+++ wsn-api/downloads/wsn-api_class/wsn-api_class/wsn-api/config.php
@@ -0,0 +1,16 @@
+<?php
+	/***********************************************************
+	 *  NOTE: edit this config details with correct credentials 
+	 ***********************************************************/
+	 
+	//Set the account email
+	define('USER_EMAIL', 'your-email-address');
+
+	//set the service password
+	define('PASSWORD', 'your-password');
+
+	//set the api key
+	define('API_KEY', 'you-api-key');
+
+?>
+
